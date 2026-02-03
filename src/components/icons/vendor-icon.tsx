eddb10c925f67a0b1e2f4f13a1dfccfcb64449e2
Lauren Florek <laurengoedhart@gmail.com>
2026-02-03 23:11:00 +0000

    here is the one for vendor:<svg xmlns="http://www.w3.org/2000/svg" xmlns

--- src/components/icons/vendor-icon.tsx
+++ src/components/icons/vendor-icon.tsx
@@ -1,15 +1,65 @@
 "use client";
 import type { SVGProps } from 'react';
-import { Store } from 'lucide-react';
 
-/**
- * To use your own SVG, open your vendor.svg file in a text editor, copy the <svg> element,
- * and paste it here, replacing the existing <svg> element.
- * Make sure to add {...props} to the <svg> element to pass down styles.
- */
 export function VendorIcon(props: SVGProps<SVGSVGElement>) {
-  // This is a placeholder icon.
   return (
-    <Store {...props} />
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="2000"
+      height="2000"
+      viewBox="0 0 1500 1500"
+      {...props}
+    >
+      <g>
+        <path
+          fill="currentColor"
+          d="M63.77 58.71h5.64v1382.58h-5.64z"
+        />
+        <path
+          fill="currentColor"
+          d="M337.15 58.71h5.64v1382.58h-5.64z"
+        />
+        <path
+          fill="currentColor"
+          d="M610.53 58.71h5.64v1382.58h-5.64z"
+        />
+        <path
+          fill="currentColor"
+          d="M883.83 58.71h5.64v1382.58h-5.64z"
+        />
+        <path
+          fill="currentColor"
+          d="M1157.21 58.71h5.64v1382.58h-5.64z"
+        />
+        <path
+          fill="currentColor"
+          d="M1430.59 58.71h5.64v1382.58h-5.64z"
+        />
+        <path
+          fill="currentColor"
+          d="M63.85 55.88h1372.38v5.65H63.85z"
+        />
+        <path
+          fill="currentColor"
+          d="M63.85 332.4h1372.38v5.65H63.85z"
+        />
+        <path
+          fill="currentColor"
+          d="M63.85 608.91h1372.38v5.65H63.85z"
+        />
+        <path
+          fill="currentColor"
+          d="M63.85 885.43h1372.38v5.65H63.85z"
+        />
+        <path
+          fill="currentColor"
+          d="M63.85 1161.95h1372.38v5.64H63.85z"
+        />
+        <path
+          fill="currentColor"
+          d="M63.85 1438.46h1372.38v5.65H63.85z"
+        />
+      </g>
+    </svg>
   );
 }
